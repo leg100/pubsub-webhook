2210e1d008ca481ecf0be9838a2d26cc0ffa528a
Louis Garman <75728+leg100@users.noreply.github.com>
2019-10-08 10:52:49 +0100

    Code and tests

--- README.md
+++ README.md
@@ -2,6 +2,66 @@
 
 Convert webhooks requests to PubSub messages.
 
-This is a Cloud Function that takes an incoming HTTP POST payload and forwards it to a Pub/Sub topic. That's it. It's particularly useful if you want to serve a single webhook on Google Cloud and have it trigger multiple subscribers, whether it be Cloud Functions or App Engine applications (or anything else that can subscribe to a Pub/Sub topic).
+This is a Google Cloud Function that takes an incoming HTTP POST payload and forwards it to a Pub/Sub topic. That's it. It's particularly useful if you want to serve a single webhook on Google Cloud and have it trigger multiple subscribers, whether it be Cloud Functions or App Engine applications, or any subscribing to the topic.
 
 ![Diagram](pubsub-webhook.svg)
+
+## Requirements
+
+* Google Cloud
+
+## Installation
+
+Set some environment variables first:
+
+* `GOOGLE_CLOUD_PROJECT`: the project in which to deploy the function
+* `TOPIC_NAME`: the Pub/Sub topic to which to forward the POST payloads
+* `TOPIC_PROJECT`: (optional) the project hosting the Pub/Sub topic; the default is the same project as the function
+
+### Create Topic
+
+Create a Cloud Pub/Sub topic:
+
+```bash
+gcloud pubsub topics create $TOPIC_NAME
+```
+
+### Configure IAM
+
+Create a new service account for use by the Cloud Function:
+
+```bash
+gcloud iam service-accounts create webhook
+```
+
+Grant permissions to publish to the topic:
+
+```bash
+gcloud pubsub topics add-iam-policy-binding $TOPIC_NAME \
+    --member "serviceAccount:webhook@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com" \
+    --role roles/pubsub.publisher \
+    --project $TOPIC_PROJECT
+```
+
+### Deploy
+
+```bash
+gcloud beta functions deploy webhook \
+     --source . \
+     --runtime python37 \
+     --entry-point pubsub_webhook \
+     --service-account webhook@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com \
+     --set-env-vars TOPIC_NAME=${TOPIC_NAME},TOPIC_PROJECT=${TOPIC_PROJECT} \
+     --trigger-http \
+     --allow-unauthenticated
+```
+
+### Test
+
+Run an integration test against a deployed function:
+
+```bash
+make integration
+```
+
+Ensure you've set the environment variable `TOPIC_NAME` first.
--- tests/test_func.py
+++ tests/test_func.py
@@ -1,65 +1,9 @@
-import base64
-import json
 import flask
 import pytest
 from unittest import mock
+from google.cloud import pubsub_v1
 
 import main
-
-
-def event(func):
-    def wrapper():
-        data = func()
-        encoded = base64.b64encode(json.dumps(data).encode())
-        return {'data': encoded}
-
-    return wrapper
-
-
-@pytest.fixture
-@event
-def github_data():
-    return {
-        'sourceProvenance': {
-            'resolvedRepoSource': {
-                'commitSha': 'd985a61daddbcd9c05a06d199efc2aeca55e4a19',
-                'repoName': 'github_leg100_webapp'
-            }
-        },
-        'logUrl': 'https://console.cloud.google.com/gcr/builds/aeccd2ef-f51a-4a44-8e2e-0de2609ce367?project=292927648743',
-        'buildTriggerId': '2bceb582-3141-44bf-b444-5640cbcaecc5',
-        'status': 'SUCCESS'
-    }
-
-
-@pytest.fixture
-@event
-def bitbucket_data():
-    return {
-        'sourceProvenance': {
-            'resolvedRepoSource': {
-                'commitSha': '65ca6a99a2573f0f1ff5dc93f78a77966248ea2d',
-                'repoName': 'bitbucket_garman_webapp'
-            }
-        },
-        'logUrl': 'https://console.cloud.google.com/gcr/builds/aeccd2ef-f51a-4a44-8e2e-0de2609ce367?project=292927648743',
-        'buildTriggerId': '2bceb582-3141-44bf-b444-5640cbcaecc5',
-        'status': 'SUCCESS'
-    }
-
-
-@pytest.fixture
-@event
-def github_app_data():
-    return {
-        "sourceProvenance": {
-            "resolvedStorageSource": {
-                 "bucket": "292927648743.cloudbuild-source.googleusercontent.com",
-                 "object": "ff7f18ef55982750630698ebffed9469a2d294db-9d56424f-c12b-4f7c-923b-34b9a77fcdaf.tar.gz",
-                 "generation": "1570110452499581"
-             }
-        }
-    }
 
 
 @pytest.fixture
@@ -69,52 +13,31 @@ def env_vars(monkeypatch):
 
 
 @pytest.fixture
-def mock_client():
-    class MockClient:
-        def topic_path(self, project, topic):
-            return f'projects/{project}/topics/{topic}'
-
-
-        def get_topic(self, path):
-            return path
-
-
-        def publish(self, topic, data):
-            return None
-
-
-    return MockClient()
-
-
-def test_wrong_method(env_vars):
+def req():
     req = mock.MagicMock(spec=flask.Request)
+    req.method = 'POST'
+    req.get_data = mock.MagicMock(return_value='{"foo": "bar"}')
+
+    return req
+
+
+@pytest.fixture
+def client(mocker):
+    mock_client = mock.MagicMock(pubsub_v1.publisher.client.Client)
+    mocker.patch('main.pubsub.PublisherClient', return_value=mock_client)
+
+    return mock_client
+
+
+def test_wrong_method(req):
     req.method = 'GET'
 
     assert main.pubsub_webhook(req) == ('Method not allowed', 405)
 
 
-def test_func(mocker, env_vars):
-    req = mock.MagicMock(spec=flask.Request)
-    req.method = 'POST'
-
-    import google
-    mock_client = mock.MagicMock(google.cloud.pubsub_v1.publisher.client.Client)
-    mocker.patch('main.pubsub.PublisherClient', return_value=mock_client)
-    #class MockClient:
-    #    def topic_path(self, project, topic):
-    #        return f'projects/{project}/topics/{topic}'
-
-
-    #    def get_topic(self, path):
-    #        return path
-
-
-    #    def publish(self, topic, data):
-    #        return None
-
+def test_client(req, client, env_vars):
     assert main.pubsub_webhook(req) == 'OK'
 
-    mock_client.topic_path.assert_called_with(
-            'my-google-cloud-project',
-            'my-topic')
-
+    client.publish.assert_called_with(
+            'projects/my-google-cloud-project/topics/my-topic',
+            '{"foo": "bar"}')
